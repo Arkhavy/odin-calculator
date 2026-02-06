845d69963990791c761ff96ad0e0d7a0e68714e7
Arkhavy <arkhavy@gmail.com>
2026-02-06 14:21:21 +0100

    [CORE] Add operate function in script.js

--- script.js
+++ script.js
@@ -32,3 +32,12 @@ function divide(a, b) {
  * Function meant to call ADD, SUBSTRACT, MULTIPLY and DIVIDE function according
  * to the operator given by the user
 */
+function operate(a, operator, b) {
+	switch(operator){
+		case "+": return (add(a, b));
+		case "-": return (substract(a, b));
+		case "*": return (multiply(a, b));
+		case "/": return (divide(a, b));
+		default: return ("ERROR: Invalid operator");
+	}
+}
