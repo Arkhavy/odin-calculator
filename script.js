92ff8420f060c88df4b4dfc245b9aef05ea4fe5e
Arkhavy <arkhavy@gmail.com>
2026-02-06 16:34:37 +0100

    [CORE] Add event listeners and button logic
    Buttons now work as expected by affecting the selected number in display
    When clicking a number in display, it becomes in selected state
    The number in a selected state will be updated when a button is clicked
    UX has been added in the previous commit to see which number is selected
    Display of the result when clicking the equal button is in console

--- script.js
+++ script.js
@@ -1,3 +1,80 @@
+const numberA = document.getElementById("numberA");
+const numberB = document.getElementById("numberB");
+const mathsOperator = document.getElementById("mathsOperator");
+const clearButton = document.getElementById("clearButton");
+
+const buttonList = Array.from(document.getElementsByTagName("button"));
+const numberButtonList = Array.from(document.getElementsByClassName("number"));
+const operatorButtonList = Array.from(document.getElementsByClassName("operator"));
+// console.log(buttonList);
+// console.log(numberButtonList);
+// console.log(operatorButtonList);
+
+/* ************************************************************************** */
+/*                          operator event listeners                          */
+/* ************************************************************************** */
+operatorButtonList.forEach((button) => {
+	if (button.textContent !== "=") {
+		button.addEventListener("click", () => {
+			mathsOperator.textContent = button.textContent;
+		});
+	} else {
+		button.addEventListener("click", () => {
+			const a = parseInt(numberA.textContent);
+			const b = parseInt(numberB.textContent);
+			console.log(operate(a, mathsOperator.textContent, b));
+		})
+	}
+});
+
+/* ************************************************************************** */
+/*                           display event listeners                          */
+/* ************************************************************************** */
+numberA.selected = false;
+numberB.selected = false;
+
+numberA.addEventListener("click", () => {
+	numberB.selected = false;
+	numberB.style.borderColor = "rgb(240, 240, 240)";
+
+	numberA.selected = true;
+	numberA.style.borderColor = "rgb(125, 125, 125)";
+});
+numberB.addEventListener("click", () => {
+	numberA.selected = false;
+	numberA.style.borderColor = "rgb(240, 240, 240)";
+
+	numberB.selected = true;
+	numberB.style.borderColor = `rgb(125, 125, 125)`;
+});
+
+clearButton.addEventListener("click", () => {
+	numberA.textContent = 0;
+	numberB.textContent = 0;
+});
+
+/* ************************************************************************** */
+/*                           number event listeners                           */
+/* ************************************************************************** */
+numberButtonList.forEach((button) => {
+	button.addEventListener("click", () => {
+		if (numberA.selected) {
+			if (numberA.textContent === "0") {
+				numberA.textContent = button.textContent;
+			} else {
+				numberA.textContent += button.textContent;
+			}
+		}
+		if (numberB.selected) {
+			if (numberB.textContent === "0") {
+				numberB.textContent = button.textContent;
+			} else {
+				numberB.textContent += button.textContent;
+			}
+		}
+	});
+});
+
 /* ************************************************************************** */
 /*                                ADD function                                */
 /* ************************************************************************** */
